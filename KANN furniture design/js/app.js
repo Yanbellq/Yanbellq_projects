5dd5c7da18aba2fc9c2a100c58aab9d8908e3a7d
Yanbellq <yanbellq@gmail.com>
2025-02-13 21:57:40 +0200

    Project: 'KANN' wip: Product second section done

--- KANN furniture design/js/app.js
+++ KANN furniture design/js/app.js
@@ -1,1 +1,13 @@
 document.querySelector('#cart-count').textContent = '0';
+
+document.querySelector('.add-to-cart').addEventListener('click', function () {
+    let count = document.querySelector('#cart-count').textContent;
+    document.querySelector('#cart-count').textContent = ++count;
+});
+
+document.querySelector('#count-reset').addEventListener('click', function () {
+    let count = document.querySelector('#cart-count').textContent;
+    if (count > 0) {
+        document.querySelector('#cart-count').textContent = '0';
+    }
+});
